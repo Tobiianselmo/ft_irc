7df43eaf74507cc2c305d130491dee6ff7ecc1d6
Tobias Anselmo <tobianselmo04@gmail.com>
2025-03-04 00:23:11 +0100

    Split function modified.

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,17 +2,19 @@
 
 std::vector<std::string> split(const std::string &str, char delimiter)
 {
-    std::vector<std::string> tokens;
-    std::string::size_type start = 0;
-    std::string::size_type end = str.find(delimiter);
+	std::vector<std::string> tokens;
+	size_t start = 0;
+	size_t end = str.find(delimiter);
 
-    while (end != std::string::npos)
+	if (end != std::string::npos)
 	{
-        tokens.push_back(str.substr(start, end - start));
-        start = end + 1;
-        end = str.find(delimiter, start);
-    }
-
-    tokens.push_back(str.substr(start));
-    return tokens;
+		while (end != std::string::npos)
+		{
+			tokens.push_back(str.substr(start, end - start));
+			start = end + 1;
+			end = str.find(delimiter, start);
+		}
+		tokens.push_back(str.substr(start));
+	}
+	return tokens;
 }

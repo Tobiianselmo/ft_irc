0810b784ae17c48f8643fb06d9386e3098d6f41c
Tobias Anselmo <tanselmo@c1r9s6.42malaga.com>
2025-03-14 19:43:32 +0100

    Add responses in invite.cpp

--- src/invite.cpp
+++ src/invite.cpp
@@ -1,21 +1,36 @@
 
 #include "../include/Server.hpp"
 
-int	Server::inviteCommand(std::string line, Client &client)
+void	Server::inviteCommand(std::string line, Client &client, t_data &cmd)
 {
+	cmd.cmdType = "INVITE";
 	std::vector<std::string> parameters = split(line, ' ');
 
 	if (parameters.size() != 3)
-		return (ERR_NEEDMOREPARAMS);
+	{
+		this->createResponse(ERR_NEEDMOREPARAMS, cmd);
+		return ;
+	}
+
 	Channel	*tmp = getChannel(parameters[2]);
 	if (!tmp)
-		return (ERR_NOSUCHCHANNEL);
+	{
+		this->createResponse(ERR_NOSUCHCHANNEL, cmd);
+		return ;
+	}
 	if (tmp->isOperator(client.getNickName()) == false)
-		return (ERR_NOOPERHOST);
+	{
+		this->createResponse(ERR_NOOPERHOST, cmd);
+		return ;
+	}
+
 	Client	*clientTmp = this->getClient(parameters[1]);
 	if (!clientTmp)
-		return (ERR_NOSUCHNICK);
+	{
+		this->createResponse(ERR_NOSUCHNICK, cmd);
+		return ;
+	}
 	tmp->addInvite(*clientTmp);
 	tmp->printInvited();
-	return (0);
+	return ;
 }

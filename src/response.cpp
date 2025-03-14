d11d070df3bda2a02cbfaaf07482d76c91a8843f
Tobias Anselmo <tanselmo@c1r9s6.42malaga.com>
2025-03-14 19:46:39 +0100

    Change some conditionals for else if

--- src/response.cpp
+++ src/response.cpp
@@ -18,42 +18,33 @@ std::string rpl_namreply(Server *server, t_data &cmd, std::string err)
 	return response;
 }
 
-std::string rpl_endofnames(Server *server, t_data &cmd, std::string err)
-{
-	std::string response;
-
-	response = ":" + server->getHostName() + " " + err + " " + cmd.client->getNickName() + " " + cmd.channel->getName() + " :End of /NAMES list\r\n";
-	return response;
-}
-
 void Server::createResponse(int err, t_data &cmd)
 {
 	std::string prefix = ":ft_irc " + intToString(err) + " ";
 	std::string response;
-
 	
-	if (err == ERR_UNKNOWNCOMMAND)
+	if (err == ERR_UNKNOWNCOMMAND)  // finished
 		response = prefix + cmd.client->getNickName() + " :Unknown command\r\n";
-	else if (err == ERR_BADCHANNELKEY)
+	else if (err == ERR_BADCHANNELKEY)  // finished
 		response = prefix + cmd.client->getNickName() + " " + cmd.channelName + " :Cannot join channel (+k)\r\n";
-	else if (err == ERR_BADCHANMASK)
+	else if (err == ERR_BADCHANMASK)  // finished
 		response = prefix + cmd.channelName + " :Bad channel mask\r\n";
-	else if (err == ERR_NEEDMOREPARAMS)
+	else if (err == ERR_NEEDMOREPARAMS)  // finished
 		response = ":" + cmd.client->getNickName() + " " + cmd.cmdType + " :Not enough parameters\r\n";
-	else if (err == RPL_JOIN)
-		response = ":" + cmd.client->getNickName() + " JOIN " + cmd.channel->getName() + "\r\n";
-	else if (err == RPL_NAMREPLY)
+	else if (err == RPL_JOIN)  // finished
+		response = ":" + cmd.client->getNickName() + " JOIN " + cmd.channelName + "\r\n";
+	else if (err == RPL_NAMREPLY)  // finished
 		response = rpl_namreply(this, cmd, intToString(err));
-	else if (err == RPL_ENDOFNAMES)
-		response = rpl_endofnames(this, cmd, intToString(err));
-	// case ERR_NOSUCHCHANNEL:
-	// 	response = 	cmd.client->getNickName() + " " + cmd.channel->getName() + " :No such channel\r\n";
-	// case ERR_CHANOPRIVSNEEDED:
-	// 	response = cmd.client->getNickName() + " " + cmd.channel->getName() + " :You're not channel operator\r\n";
-	// case ERR_NOTONCHANNEL:
-	// 	response = cmd.client->getNickName() + " " + cmd.channel->getName() + " :You're not on that channel\r\n";
-	// case ERR_USERNOTINCHANNEL:
-	// 	response = cmd.client->getNickName() + " " + cmd.user + cmd.channel->getName() + " :They aren't on that channel\r\r";
+	else if (err == RPL_ENDOFNAMES) // finished
+	response = ":" + this->getHostName() + " " + intToString(err) + " " + cmd.client->getNickName() + " " + cmd.channelName + " :End of /NAMES list\r\n";
+	// else if (err == ERR_NOSUCHCHANNEL)
+	// 	response = cmd.client->getNickName() + " " + cmd.channelName + " :No such channel\r\n";
+	// else if (err == ERR_CHANOPRIVSNEEDED)
+	// 	response = cmd.client->getNickName() + " " + cmd.channelName + " :You're not channel operator\r\n";
+	// else if (err == ERR_NOTONCHANNEL)
+	// 	response = cmd.client->getNickName() + " " + cmd.channelName + " :You're not on that channel\r\n";
+	// else if (err == ERR_USERNOTINCHANNEL)
+	// 	response = cmd.client->getNickName() + " " + cmd.destUser + " " + cmd.channelName + " :They aren't on that channel\r\r";
 	else
 		response = "";
 	send(cmd.client->getClientSocket(), response.c_str(), response.size(), 0);

72330a2706bf524a1cb98e26bee111634ec91483
Tobias Anselmo <tanselmo@c1r9s6.42malaga.com>
2025-03-14 17:13:41 +0100

    Client Class refactored.

--- include/Client.hpp
+++ include/Client.hpp
@@ -8,27 +8,32 @@ class Server;
 class Client
 {
 	private:
-		int _clientSocket;
-		std::string _nickName;
-		std::string _userName;
-		bool	_isAuth;
+		int			_clientSocket;
+		std::string	_nickName;
+		std::string	_userName;
+		bool		_isAuth;
+		bool		_hasCorrectPass;
 
 		Server *_server;
 	public:
 		Client();
 		~Client();
-		Client &operator=(const Client &other);
 		Client(const Client &other);
+		Client &operator=(const Client &other);
 		Client(int socket, Server &server);
 
+		// Setters
 		void	setNickName(std::string nickname);
 		void	setUserName(std::string username);
 		void	setAuth(bool val);
+		void	setCorrectPass(bool val);
 
+		// Getters
 		const std::string	&getNickName() const;
 		const std::string	&getUserName() const;
 		int					getClientSocket() const;
 		bool				isAuth() const;
+		bool				hasCorrectPass() const;
 };
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -28,6 +28,7 @@ Client &Client::operator=(const Client &other)
 Client::Client(int socket, Server &server)
 {
 	this->_isAuth = false;
+	this->_hasCorrectPass = false;
 	this->_nickName = "";
 	this->_userName = "";
 	this->_clientSocket = socket;
@@ -39,10 +40,12 @@ Client::Client(int socket, Server &server)
 void	Client::setNickName(std::string nickname) { this->_nickName = nickname; }
 void	Client::setUserName(std::string username) { this->_userName = username; }
 void	Client::setAuth(bool val) { this->_isAuth = val; }
+void	Client::setCorrectPass(bool val) { this->_hasCorrectPass = val; }
 
 // Getters
 
-const std::string &Client::getNickName() const { return this->_nickName; }
-const std::string &Client::getUserName() const { return this->_userName; }
-int	Client::getClientSocket() const { return this->_clientSocket; }
-bool Client::isAuth() const { return this->_isAuth; }
+const std::string	&Client::getNickName() const { return this->_nickName; }
+const std::string	&Client::getUserName() const { return this->_userName; }
+int					Client::getClientSocket() const { return this->_clientSocket; }
+bool				Client::isAuth() const { return this->_isAuth; }
+bool				Client::hasCorrectPass() const { return this->_hasCorrectPass; }

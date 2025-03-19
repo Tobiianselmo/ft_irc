8ffdea29f40f889583da917be9b413db2f0e50d4
Matias Eduardo Isaguirre Escobar <misaguir@c2r4s5.42malaga.com>
2025-03-19 15:19:09 +0100

    new split

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,23 +2,20 @@
 
 std::vector<std::string> split(const std::string &str, char delimiter)
 {
-	std::vector<std::string> tokens;
-	size_t start = 0;
-	size_t end = str.find(delimiter);
+    std::vector<std::string> tokens;
+    size_t start = 0;
+    size_t end = str.find(delimiter);
 
-	if (end != std::string::npos)
+    while (end != std::string::npos)
 	{
-		while (end != std::string::npos)
-		{
-			tokens.push_back(str.substr(start, end - start));
-			start = end + 1;
-			end = str.find(delimiter, start);
-		}
-		tokens.push_back(str.substr(start));
-	}
-	else
-		tokens.push_back(str);
-	return tokens;
+        if (start != end)
+            tokens.push_back(str.substr(start, end - start));
+        start = end + 1;
+        end = str.find(delimiter, start);
+    }
+    if (start < str.size())
+        tokens.push_back(str.substr(start));
+    return tokens;
 }
 
 const char *checkNickName(const char *str)

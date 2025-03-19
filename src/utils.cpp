2e69dde247bdca09d7c712256431726a8d268c0d
Tobias Anselmo <tanselmo@c2r4s3.42malaga.com>
2025-03-19 17:55:39 +0100

    Fix the split function

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,18 +6,15 @@ std::vector<std::string> split(const std::string &str, char delimiter)
 	size_t start = 0;
 	size_t end = str.find(delimiter);
 
-	if (end != std::string::npos)
+	while (end != std::string::npos)
 	{
-		while (end != std::string::npos)
-		{
+		if (start != end)
 			tokens.push_back(str.substr(start, end - start));
-			start = end + 1;
-			end = str.find(delimiter, start);
-		}
-		tokens.push_back(str.substr(start));
+		start = end + 1;
+		end = str.find(delimiter, start);
 	}
-	else
-		tokens.push_back(str);
+	if (start < str.size())
+		tokens.push_back(str.substr(start));
 	return tokens;
 }
 

8232511ca36b50ca3e71d58944cc5ffba7f19d46
tanselmo <tobianselmo04@gmail.com>
2025-03-24 17:17:07 +0100

    All the responses added, but some doubts with the ERR_USERONCHANNEL response, because the client receives the response in the server's lobby and not in the channel, but i think its ok.

--- src/cmd/invite.cpp
+++ src/cmd/invite.cpp
@@ -3,6 +3,7 @@
 void	Server::inviteCommand(std::string line, Client &client, t_data &cmd)
 {
 	cmd.cmdType = "INVITE";
+	cmd.msg = commandToUpper(line);
 	std::vector<std::string> parameters = split(line, ' ');
 
 	if (parameters.size() != 3)
@@ -11,6 +12,7 @@ void	Server::inviteCommand(std::string line, Client &client, t_data &cmd)
 		return ;
 	}
 
+	cmd.channelName = parameters[2];
 	Channel	*tmp = getChannel(parameters[2]);
 	if (!tmp)
 	{
@@ -23,13 +25,19 @@ void	Server::inviteCommand(std::string line, Client &client, t_data &cmd)
 		return ;
 	}
 
+	cmd.destUser = parameters[1];
 	Client	*clientTmp = this->getClient(parameters[1]);
 	if (!clientTmp)
-	{
 		this->createResponse(ERR_NOSUCHNICK, cmd, ONLY_CLIENT);
-		return ;
+	else
+	{
+		if (tmp->isClient(clientTmp->getNickName()) == true)
+			this->createResponse(ERR_USERONCHANNEL, cmd, ONLY_CLIENT);
+		else
+		{
+			cmd.destUser = clientTmp->getNickName();
+			tmp->addInvite(clientTmp);
+			this->createResponse(RPL_INVITING, cmd, ONE_CLIENT);
+		}
 	}
-	tmp->addInvite(clientTmp);
-	tmp->printInvited();
-	return ;
 }

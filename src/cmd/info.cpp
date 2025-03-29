055211bbcd8116300ff9d5a47b416b757c672176
Delfina Maria Luz Parada <dparada@c1r6s3.42malaga.com>
2025-03-29 12:39:23 +0100

    added modes description

--- src/cmd/info.cpp
+++ src/cmd/info.cpp
@@ -5,7 +5,7 @@ void	Server::infoCommand(t_data &cmd)
 	send(cmd.client->getClientSocket(),"cmd join or JOIN: join a channel\n", 33, 0);
 	send(cmd.client->getClientSocket(),"cmd kick or KICK: kicks a user from a channel\n", 46, 0);
 	send(cmd.client->getClientSocket(),"cmd nick or NICK: change nickname\n", 34, 0);
-	send(cmd.client->getClientSocket(),"cmd mode or MODE: \n", 19, 0);
+	send(cmd.client->getClientSocket(),"cmd mode or MODE: modify the channel's modes\n", 45, 0);
 	send(cmd.client->getClientSocket(),"cmd invite or INVITE: invites user to a channel\n", 48, 0);
 	send(cmd.client->getClientSocket(),"cmd topic or TOPIC: change the channel topic\n", 45, 0);
 	send(cmd.client->getClientSocket(),"cmd user or USER: change username\n", 34, 0);

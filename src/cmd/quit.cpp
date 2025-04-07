46b8fee4ad143584c348b57f264a7bd15f8a1396
lordmati <matias.isaguirre@hotmail.com>
2025-04-07 16:29:25 +0200

    quit update

--- src/cmd/quit.cpp
+++ src/cmd/quit.cpp
@@ -4,30 +4,22 @@ void	Server::quitCommand(std::string line, Client &client, t_data &cmd)
 {
 	cmd.cmdType = "QUIT";
 	cmd.msg = commandToUpper(line);
+	int aux;
 	std::vector<std::string>	parameters = split(line, ' ');
 
 	for (size_t i = 0; i < this->_fds.size(); i++)
 	{
 		if (this->_fds[i].fd == client.getClientSocket())
 		{
-			this->_fds.erase(this->_fds.begin() + i);
+			aux = i;
 			break ;
 		}
 	}
 	for (int i = this->_channels.size() - 1; i >= 0; i--)
 	{
 		if (this->_channels[i].isClient(client.getNickName()) == true)
-		{
-			cmd.channel = &this->_channels[i];
 			this->createResponse(RPL_QUIT, cmd, ALL_CHANNEL);
-			this->_channels[i].deleteClient(&client);
-		}
-		if (this->_channels[i].getUserSize() == 0)
-		this->_channels.erase(_channels.begin() + i);
 	}
-	int fd = client.getClientSocket();
-	close(client.getClientSocket());
-	delete _clientsMap[fd];
+	this->remClientFromServ(client,aux);
 	std::cout << "Client disconected" << std::endl;
-	_clientsMap.erase(fd);
 }
